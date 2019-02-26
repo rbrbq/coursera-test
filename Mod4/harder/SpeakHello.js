11e47dbcbe66836417d347059ca7ddcf354fc175
Qu Rui <1060351138@qq.com>
2019-02-26 22:37:02 +0800

    ok

---
+++ Mod4/harder/SpeakHello.js
@@ -0,0 +1,8 @@
+(function (window) {
+	var speakWord = "Hello";
+	var helloSpeaker = function speak(name) {
+  	console.log(speakWord + " " + name);
+} 
+window.helloSpeaker = helloSpeaker;
+})(window)
+
